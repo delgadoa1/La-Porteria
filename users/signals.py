a248b5c9ae6b05c0c44f1157224cf9d23592ca8e
delgadoa1 <njandrenj@gmail.com>
2019-03-31 21:25:20 -0400

    Added more user functionality, and profile pics for user posts on home page

---
+++ users/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+"""
+When a User is saved, send signal received by receiver. Receiver is create_profile func that takes 
+all arguments that post_save signal passes. If that user is created, create a profile object with 
+user = instance of profile created
+"""
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):     # Create a profile crated for each new user
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_post(sender, instance, created, **kwargs):
+    instance.profile.save()
+
